968c0ebfeb2be1ae87148a464615a86efe552cf7
Sohail Shaikh <sohail28195@gmail.com>
2020-06-08 11:23:33 +0530

    View router added made http requests

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,16 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
-
 <script>
 import Header from "./components/layout/Header";
-import AddTodo from "./components/AddTodo";
-import Todos from "./components/Todos";
 export default {
-  name: "app",
-  components: { Header, AddTodo, Todos },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false,
-        },
-      ],
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter((todo) => todo.id !== id);
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
-    },
+  name: "App",
+  components: {
+    Header,
   },
 };
 </script>
-
-<style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
-}
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
-  color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
-}
-.btn:hover {
-  background: #666;
-}
-</style>
+<style></style>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,16 +1,16 @@
 <template>
   <header class="header">
     <h1>TodoList</h1>
-    <!-- <div id="nav">
+    <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
-    </div>-->
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
 };
 </script>
 
